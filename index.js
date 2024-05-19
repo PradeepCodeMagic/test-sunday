15080761c6a951f85cbade925b2e6f44e98fc41a
Pradeep <143385717+PradeepCodeMagic@users.noreply.github.com>
2024-05-19 11:32:32 +0530

    hello

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(express.json());
 app.use(allRoutes);
 
+app.get('/', (req,res)=>{
+    res.send('hello');
+});
+
 app.listen(5000, ()=>{
     console.log('server is running on port 5000');
 });
